b80864028dc4af3bb64f2496d4427c3db302f740
BrianSeng <brianseng.dev@gmail.com>
2019-05-23 13:27:59 -0700

    Utilized TimeProcessor.CurrentTimeUTC method in handler's Get() method. Also added a CreateResponse utility method for returning a proper APIGatewayProxyResponse to the caller.

--- src/DebuggingExample/Function.cs
+++ src/DebuggingExample/Function.cs
@@ -3,39 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-
-using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace DebuggingExample
-{
-    public class Functions
-    {
+namespace DebuggingExample {
+    public class Functions {
+        ITimeProcessor processor = new TimeProcessor();
+
         /// <summary>
         /// Default constructor that Lambda will invoke.
         /// </summary>
-        public Functions()
-        {
-        }
-
+        public Functions () { }
 
         /// <summary>
         /// A Lambda function to respond to HTTP Get methods from API Gateway
         /// </summary>
-        /// <param name="request"></param>
+        /// <param name="request">The http request that triggered this lambda function. Contains the request body, http headers, etc.</param>
+        /// <param name="context">AWS context object passed to handler. Contains props w/ info about the invocation, function, and execution env</param>
         /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
-        {
-            context.Logger.LogLine("Get Request\n");
+        public APIGatewayProxyResponse Get (APIGatewayProxyRequest request, ILambdaContext context) {
+            var result = processor.CurrentTimeUTC();
 
-            var response = new APIGatewayProxyResponse
-            {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = "Hello AWS Serverless",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            return CreateResponse(result);
+        }
+
+        APIGatewayProxyResponse CreateResponse (DateTime? result) {
+            int statusCode = (result != null) ?
+                (int)HttpStatusCode.OK : (int)HttpStatusCode.InternalServerError;
+
+            string body = (result != null) ?
+                JsonConvert.SerializeObject(result) : string.Empty;
+
+            var response = new APIGatewayProxyResponse {
+                StatusCode = statusCode,
+                Body = body,
+                Headers = new Dictionary<string, string> { 
+                    { "Content-Type", "application/json" },
+                    { "Access-Control-Allow-Origin", "*" }
+                }
             };
 
             return response;
